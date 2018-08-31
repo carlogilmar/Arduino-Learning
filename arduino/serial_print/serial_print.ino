68a93cc31c25cccd0ca9ba7a2bc6b885138d0f77
carlogilmar <carlogilmar12@gmail.com>
2018-08-30 23:51:17 -0500

    Adding example for test elixir uart

---
+++ arduino/serial_print/serial_print.ino
@@ -0,0 +1,16 @@
+void setup(){
+ Serial.begin(9600); 
+}
+
+void loop(){
+  delay(500);
+  Serial.print("1\n");
+  delay(500);
+  Serial.print("2\n");
+  delay(500);
+  Serial.print("3\n");
+  delay(500);
+  Serial.print("4\n");
+  delay(500);
+  Serial.print("5\n");
+}
